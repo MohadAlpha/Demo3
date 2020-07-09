9344c7b1b7c07e2d40ea0cdba09e80af8ab6e052
MohadAlpha <67737211+MohadAlpha@users.noreply.github.com>
2020-07-09 12:39:44 +0300

    Add files via upload

---
+++ Lab1_0.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+class task1
+
+{
+    double Base , Height;
+    task1()
+    {
+    	Base  = 0;
+    	Height = 0;
+    }
+    task1(double Base , double Height)
+    {
+        this.Base  = Base ;
+        this.Height = Height;
+       
+    }
+    double area=(Base*Height)/2;
+    
+task1(task1 x)
+    {
+        this(x.Base,x.Height);
+    }
+
+public double toDouble()
+{
+    return area;
+}     
+}
+
+
+
+
+
+
+
+public class Lab1_0 {
+
+
+	
+	
+	public static void main(String[] args) {
+		
+		task1 tre = new task1();
+        tre.Base = 5; 
+        tre.Height = 9; 
+        System.out.println(tre);
+
+	}
+
+}
